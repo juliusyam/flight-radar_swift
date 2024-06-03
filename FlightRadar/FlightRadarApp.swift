a8bfd8083a2c3b4d72ef8e40daaba56eeb18c1a7
Kabba Joseph Timothy <kabbajosephtimothy@gmail.com>
2024-06-03 15:29:14 +0300

    Initial Commit

---
+++ FlightRadar/FlightRadarApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightRadarApp.swift
+//  FlightRadar
+//
+//  Created by KABBA JOSEPH TIMOTHY on 03/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct FlightRadarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
